1ad61d8d7cc778e5924fbe0ff384d7a331313432
Maksimilian Romanov <maks.romanov.15@inbox.ru>
2023-01-31 11:32:46 +0300

    version 1.0

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,12 +1,27 @@
+import django_filters.rest_framework
+from django_filters import DateFromToRangeFilter, rest_framework
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+
+class AdvertisementFilter(rest_framework.FilterSet):
+    created_at = DateFromToRangeFilter()
+
+    class Meta:
+        model = Advertisement
+        fields = ['title', 'description', 'status', 'creator', 'created_at']
 
 
 class AdvertisementViewSet(ModelViewSet):
-    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [rest_framework.DjangoFilterBackend]
+    filterset_class = AdvertisementFilter
+    filterset_fields = ["created_at", "creator"]
 
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
+    def perfom_create(self, serializer):
+        serializer.save(creator=self.request.creator)
 
     def get_permissions(self):
         """Получение прав для действий."""
--- 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
+++ 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
@@ -74,9 +74,20 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '20/minute',
+        'anon': '10/minute',
+    }
 }
 
 WSGI_APPLICATION = 'api_with_restrictions.wsgi.application'
@@ -88,9 +99,11 @@ WSGI_APPLICATION = 'api_with_restrictions.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_classified_ads',
+        'NAME': 'django_lesson_7_test_1',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': '89242565295M',
     }
 }
 
